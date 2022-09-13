6327f6ecd37b6e3d82378de6f65f7bca5530c98e
Liza-Cherevko <71131183+Liza-Cherevko@users.noreply.github.com>
2022-09-13 10:53:21 +0300

    commit

--- HW-6/hw-6.js
+++ HW-6/hw-6.js
@@ -28,10 +28,13 @@ const students = [
 ]
 
 let marks = calculateStudentAverageMark (students[2]);
-console.log(marks)
+console.log('average mark one student:', marks)
+let globalAvarageMarks = calculateGroupAverageMark(students);
 let allMarks = getAllMarks(students[0],students[1],students[2],students[3])
-let overalMark = calculateGroupAverageMark(students);
+// let overalMark = calculateGroupAverageMark(allMarks);
+// console.log('average mark all students:', overalMark)
 
+// one student
  function calculateStudentAverageMark (studentsMark){
    return studentsMark.marks.reduce((sum, current) => sum + current,0)/studentMarkLenght(students);
  }
@@ -39,20 +42,10 @@ let overalMark = calculateGroupAverageMark(students);
      let studentArr = students.find(item => item.id == 12);
      return studentArr.marks.length
  }
-
- function AllstudentMarkLenght(students){
-    let allLenght = students.map(item => {
-        let container = {};
-    
-        container[item.marks] = item.marks.length;
-      
-        return container;
-    })
-    console.log(allLenght)
-    return allLenght
-}
  
-function getAllMarks(val1,val2,val3,val4){
+
+// all students
+function getAllMarks(val1, val2, val3, val4) {
     val1 = val1.marks.reduce((sum, current) => sum + current,0);
     val2 = val2.marks.reduce((sum, current) => sum + current,0);
     val3 = val3.marks.reduce((sum, current) => sum + current,0);
@@ -60,17 +53,16 @@ function getAllMarks(val1,val2,val3,val4){
   return val1+val2+val3+val4;
 }
 
- function calculateGroupAverageMark(students){
-  return result = allMarks/19
- }
+// function calculateGroupAverageMark(allMarks) {
+//   return result = allMarks/19
+//  }
+
+function calculateGroupAverageMark(students) { 
+    let map = new Map(Object.entries(students));
+
+    console.log(map.get('marks'));
+}
 
 
 
 
-
-// function calculateStudentAverageMark(students) {
-
-// }
-// function calculateGroupAverageMark(students) { 
-
-// }

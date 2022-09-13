9df87c986f411dddb1e2894253c0aff75feeb612
Liza-Cherevko <71131183+Liza-Cherevko@users.noreply.github.com>
2022-09-13 11:26:38 +0300

    commit

--- HW-6/hw-6.js
+++ HW-6/hw-6.js
@@ -29,10 +29,10 @@ const students = [
 
 let marks = calculateStudentAverageMark (students[2]);
 console.log('average mark one student:', marks)
-let globalAvarageMarks = calculateGroupAverageMark(students);
-let allMarks = getAllMarks(students[0],students[1],students[2],students[3])
-// let overalMark = calculateGroupAverageMark(allMarks);
-// console.log('average mark all students:', overalMark)
+let allMarks = getAllMarks(students)
+let globalAvarageMarks = calculateGroupAverageMark();
+
+console.log('average all mark one student:', globalAvarageMarks)
 
 // one student
  function calculateStudentAverageMark (studentsMark){
@@ -42,26 +42,32 @@ let allMarks = getAllMarks(students[0],students[1],students[2],students[3])
      let studentArr = students.find(item => item.id == 12);
      return studentArr.marks.length
  }
+
+    function getAllMarks(students) {
+        return students.map(el => el.marks).flat(Infinity);
+    //    return allMarksArr.reduce((sum, current) => sum + current).split(',').map(Number);
+     
+     }
+function calculateGroupAverageMark() { 
+    return allMarks.reduce((total, amount) => total + amount)/allMarks.length
+}
  
 
-// all students
-function getAllMarks(val1, val2, val3, val4) {
-    val1 = val1.marks.reduce((sum, current) => sum + current,0);
-    val2 = val2.marks.reduce((sum, current) => sum + current,0);
-    val3 = val3.marks.reduce((sum, current) => sum + current,0);
-    val4 = val4.marks.reduce((sum, current) => sum + current,0);
-  return val1+val2+val3+val4;
-}
 
-// function calculateGroupAverageMark(allMarks) {
-//   return result = allMarks/19
-//  }
 
-function calculateGroupAverageMark(students) { 
-    let map = new Map(Object.entries(students));
 
-    console.log(map.get('marks'));
-}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

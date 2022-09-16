c983407f20512f96f8a09b248b26844d72e81b19
Liza-Cherevko <71131183+Liza-Cherevko@users.noreply.github.com>
2022-09-16 13:12:28 +0300

    commit

--- HW-7/hw-7.js
+++ HW-7/hw-7.js
@@ -1,14 +1,49 @@
 
 
-function createCalculator(somevalue) {
-        return {
-            add: (secondValue) =>  somevalue + secondValue,
-            sub: (secondValue) => somevalue - secondValue,
-            mult: (secondValue) => somevalue * secondValue,
-            div: (secondValue) => somevalue / secondValue,
-            set: (newValue) => newValue+(somevalue === 0)
-        }
+// function createCalculator(somevalue) {
+//     let secondValue = 0;
+//         return {
+//             add: (secondValue) => somevalue + secondValue,
+//             sub: (secondValue) =>  somevalue - secondValue,
+//             mult: (secondValue) => somevalue * secondValue,
+//             div: (secondValue) => somevalue / secondValue,
+//             set: (newValue) => newValue+(somevalue = 0)
+//         }
     
-};
+// };
+
+function createCalculator(result) {
+    return {
+    
+        add: function (val) {
+            result = result + val;
+            return result;
+          },
+          sub: function (val) {
+            result = result - val;
+            return result;
+          },
+        mul: function (val) {
+            result = result * val;
+            return result;
+        },
+        div: function (val) {
+            result = result / val;
+            return result;
+        },
+        set: function (val) {
+            result =  val;
+            return result;
+        },
+        sum: function (...args) {
+            result = result + args.reduce((pre, val) => {
+              return pre+ val;
+            }, 0);
+            return result;
+          },
+    }
+
+}
+
 
 let calc = createCalculator(100)

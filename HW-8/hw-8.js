0e3af92242b0f7051a2e6cfd5b68255fb431778d
Liza-Cherevko <71131183+Liza-Cherevko@users.noreply.github.com>
2022-09-19 15:09:25 +0300

    commit

--- HW-8/hw-8.js
+++ HW-8/hw-8.js
@@ -1,65 +1,118 @@
+let calc = document.getElementById('calculate');
 
+calc.eddEvntListner('click', getCalc)
 
-let calc = document.getElementById("calculate");
-
-calc.addEventListener("click", calculate)
-
-function calculate(){
-let act = document.getElementById("action");
-act.addEventListener("change" , showAction())
-    function add(){
-       let n1,n2,res;
-        n1=document.getElementById("num1").value;
-        n2=document.getElementById("num2").value;
-        res=parseInt(n1)+parseInt(n2);
-        document.getElementById("result").value=res;
+function getCalc() { 
+    let act = document.getElementById('action');
+    act.eddEvntListner('change', getAction())
+    function add() { 
+        let n1, n2, res;
+        n1 = document.getElementById('num1').value;
+        n2 = document.getElementById('num1').value;
+        res = parseInt(n1) + parseInt(n2);
+        document.getElementById('result').value = res;
     }
-
-    function sub(){
-        let n1,n2,res;
-        n1=document.getElementById("num1").value;
-        n2=document.getElementById("num2").value;
-        res=parseInt(n1)-parseInt(n2);
-        document.getElementById("result").value=res;
+    function sub() { 
+        let n1, n2, res;
+        n1 = document.getElementById('num1').value;
+        n2 = document.getElementById('num1').value;
+        res = parseInt(n1) - parseInt(n2);
+        document.getElementById('result').value = res;
     }
-
-    function divide(){
-        let n1,n2,res;
-        n1=document.getElementById("num1").value;
-        n2=document.getElementById("num2").value;
-        res=parseInt(n1) / parseInt(n2);
-        document.getElementById("result").value=res;
+    function div() { 
+        let n1, n2, res;
+        n1 = document.getElementById('num1').value;
+        n2 = document.getElementById('num1').value;
+        res = parseInt(n1) / parseInt(n2);
+        document.getElementById('result').value = res;
     }
-
-    function mul(){
-        let n1,n2,res;
-        n1=document.getElementById("num1").value;
-        n2=document.getElementById("num2").value;
-        res=parseInt(n1)*parseInt(n2);
-        document.getElementById("result").value=res;
+    function mul() { 
+        let n1, n2, res;
+        n1 = document.getElementById('num1').value;
+        n2 = document.getElementById('num1').value;
+        res = parseInt(n1) * parseInt(n2);
+        document.getElementById('result').value = res;
     }
+    function getAction() { 
+        switch (act.value) { 
+            case 'add':
+                add()
+                break;
+         case 'sub':
+            sub()
+                break;
+            case 'div':
+                div()
+                break;
+            case 'mul':
+                mul()
+                break;
+        }
+    }
+}
+
+
+
+// let calc = document.getElementById("calculate");
+
+// calc.addEventListener("click", calculate)
+
+// function calculate(){
+// let act = document.getElementById("action");
+// act.addEventListener("change" , showAction())
+//     function add(){
+//        let n1,n2,res;
+//         n1=document.getElementById("num1").value;
+//         n2=document.getElementById("num2").value;
+//         res=parseInt(n1)+parseInt(n2);
+//         document.getElementById("result").value=res;
+//     }
+
+//     function sub(){
+//         let n1,n2,res;
+//         n1=document.getElementById("num1").value;
+//         n2=document.getElementById("num2").value;
+//         res=parseInt(n1)-parseInt(n2);
+//         document.getElementById("result").value=res;
+//     }
+
+//     function divide(){
+//         let n1,n2,res;
+//         n1=document.getElementById("num1").value;
+//         n2=document.getElementById("num2").value;
+//         res=parseInt(n1) / parseInt(n2);
+//         document.getElementById("result").value=res;
+//     }
+
+//     function mul(){
+//         let n1,n2,res;
+//         n1=document.getElementById("num1").value;
+//         n2=document.getElementById("num2").value;
+//         res=parseInt(n1)*parseInt(n2);
+//         document.getElementById("result").value=res;
+//     }
     
 
-    function showAction(){
-     switch(act.value){
-        case "add" :
-            add()
-            break;
+//     function showAction(){
+//      switch(act.value){
+//         case "add" :
+//             add()
+//             break;
 
-        case "sub" :
-            sub()
-            break;
+//         case "sub" :
+//             sub()
+//             break;
                 
-        case "divide" :
-            divide()
-            break;
+//         case "divide" :
+//             divide()
+//             break;
                 
-        case "mul":
-            mul()
-            break;
-    }
-  }
-}
+//         case "mul":
+//             mul()
+//             break;
+//     }
+//   }
+// }
 
 
 

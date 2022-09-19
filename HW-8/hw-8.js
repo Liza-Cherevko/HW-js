2ea7eb710112dcdb3ffb8b6ed821d78fe26146ac
Liza-Cherevko <71131183+Liza-Cherevko@users.noreply.github.com>
2022-09-19 15:10:36 +0300

    commit

--- HW-8/hw-8.js
+++ HW-8/hw-8.js
@@ -1,10 +1,10 @@
 let calc = document.getElementById('calculate');
 
-calc.eddEvntListner('click', getCalc)
+calc.addEventListener('click', getCalc)
 
 function getCalc() { 
     let act = document.getElementById('action');
-    act.eddEvntListner('change', getAction())
+    act.addEventListener('change', getAction())
     function add() { 
         let n1, n2, res;
         n1 = document.getElementById('num1').value;
@@ -53,66 +53,6 @@ function getCalc() {
 
 
 
-// let calc = document.getElementById("calculate");
-
-// calc.addEventListener("click", calculate)
-
-// function calculate(){
-// let act = document.getElementById("action");
-// act.addEventListener("change" , showAction())
-//     function add(){
-//        let n1,n2,res;
-//         n1=document.getElementById("num1").value;
-//         n2=document.getElementById("num2").value;
-//         res=parseInt(n1)+parseInt(n2);
-//         document.getElementById("result").value=res;
-//     }
-
-//     function sub(){
-//         let n1,n2,res;
-//         n1=document.getElementById("num1").value;
-//         n2=document.getElementById("num2").value;
-//         res=parseInt(n1)-parseInt(n2);
-//         document.getElementById("result").value=res;
-//     }
-
-//     function divide(){
-//         let n1,n2,res;
-//         n1=document.getElementById("num1").value;
-//         n2=document.getElementById("num2").value;
-//         res=parseInt(n1) / parseInt(n2);
-//         document.getElementById("result").value=res;
-//     }
-
-//     function mul(){
-//         let n1,n2,res;
-//         n1=document.getElementById("num1").value;
-//         n2=document.getElementById("num2").value;
-//         res=parseInt(n1)*parseInt(n2);
-//         document.getElementById("result").value=res;
-//     }
-    
-
-//     function showAction(){
-//      switch(act.value){
-//         case "add" :
-//             add()
-//             break;
-
-//         case "sub" :
-//             sub()
-//             break;
-                
-//         case "divide" :
-//             divide()
-//             break;
-                
-//         case "mul":
-//             mul()
-//             break;
-//     }
-//   }
-// }
 
 
 

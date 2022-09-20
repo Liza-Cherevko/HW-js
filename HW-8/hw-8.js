83a7a6d609665405cc8e1a1603dea51f33a569a4
Liza-Cherevko <71131183+Liza-Cherevko@users.noreply.github.com>
2022-09-20 16:50:23 +0300

    commit

--- HW-8/hw-8.js
+++ HW-8/hw-8.js
@@ -1,54 +1,48 @@
+
+
+
 let calc = document.getElementById('calculate');
 
 calc.addEventListener('click', getCalc)
 
 function getCalc() { 
-    let act = document.getElementById('action');
-    act.addEventListener('change', getAction())
+    let act = document.getElementById('action').value,
+        res;
+        n1 = Number(document.getElementById('num1').value)
+        n2 = Number(document.getElementById('num2').value)
     function add() { 
-        let n1, n2, res;
-        n1 = document.getElementById('num1').value;
-        n2 = document.getElementById('num1').value;
-        res = parseInt(n1) + parseInt(n2);
-        document.getElementById('result').value = res;
+        return n1 + n2;
     }
     function sub() { 
-        let n1, n2, res;
-        n1 = document.getElementById('num1').value;
-        n2 = document.getElementById('num1').value;
-        res = parseInt(n1) - parseInt(n2);
-        document.getElementById('result').value = res;
+     
+        return n1 - n2;
     }
     function div() { 
-        let n1, n2, res;
-        n1 = document.getElementById('num1').value;
-        n2 = document.getElementById('num1').value;
-        res = parseInt(n1) / parseInt(n2);
-        document.getElementById('result').value = res;
+       
+        return n1 / n2;
     }
     function mul() { 
-        let n1, n2, res;
-        n1 = document.getElementById('num1').value;
-        n2 = document.getElementById('num1').value;
-        res = parseInt(n1) * parseInt(n2);
-        document.getElementById('result').value = res;
+      
+        return n1 * n2;
+        
     }
-    function getAction() { 
-        switch (act.value) { 
-            case 'add':
-                add()
-                break;
-         case 'sub':
-            sub()
-                break;
-            case 'div':
-                div()
-                break;
-            case 'mul':
-                mul()
-                break;
-        }
+
+    switch (act) { 
+        case 'add':
+            res = add()
+            break;
+        case 'sub':
+            res = sub()
+            break;
+        case 'div':
+            res = div()
+            break;
+        case 'mul':
+            res = mul()
+            break;
     }
+
+    document.getElementById('result').value = res;
 }
 
 
@@ -57,12 +51,55 @@ function getCalc() {
 
 
 
+// let calc = document.getElementById('calculate');
 
+// calc.addEventListener('click', getCalc)
 
-
-
-
-
-
-
-    
+// function getCalc() { 
+//     let act = document.getElementById('action');
+//     act.addEventListener('change', getAction())
+//     function add() { 
+//         let n1, n2, res;
+//         n1 = document.getElementById('num1').value;
+//         n2 = document.getElementById('num1').value;
+//         res = parseInt(n1) + parseInt(n2);
+//         document.getElementById('result').value = res;
+//     }
+//     function sub() { 
+//         let n1, n2, res;
+//         n1 = document.getElementById('num1').value;
+//         n2 = document.getElementById('num1').value;
+//         res = parseInt(n1) - parseInt(n2);
+//         document.getElementById('result').value = res;
+//     }
+//     function div() { 
+//         let n1, n2, res;
+//         n1 = document.getElementById('num1').value;
+//         n2 = document.getElementById('num1').value;
+//         res = parseInt(n1) / parseInt(n2);
+//         document.getElementById('result').value = res;
+//     }
+//     function mul() { 
+//         let n1, n2, res;
+//         n1 = document.getElementById('num1').value;
+//         n2 = document.getElementById('num1').value;
+//         res = parseInt(n1) * parseInt(n2);
+//         document.getElementById('result').value = res;
+//     }
+//     function getAction() { 
+//         switch (act.value) { 
+//             case 'add':
+//                 add()
+//                 break;
+//          case 'sub':
+//             sub()
+//                 break;
+//             case 'div':
+//                 div()
+//                 break;
+//             case 'mul':
+//                 mul()
+//                 break;
+//         }
+//     }
+// }

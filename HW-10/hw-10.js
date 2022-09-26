9b8880859853bb69f4a10d8088e1f35a73c4dd81
Liza <cherevkoelp@gmail.com>
2022-09-26 20:48:02 +0300

    commit

---
+++ HW-10/hw-10.js
@@ -0,0 +1,82 @@
+const contactListEl = document.querySelector('#contactsList');
+const nameEl = document.querySelector('#name');
+const surnameEl = document.querySelector('#surname');
+const phoneEl = document.querySelector('#phone');
+const addBtnEl = document.querySelector('#addContactBtn');
+
+addBtnEl.addEventListener('click', onAddcontactBtnclick)
+
+function onAddcontactBtnclick() { 
+    if (!validateValues()) { 
+        return;
+    }
+    const newContact = getValues();
+    addContact(newContact)
+    resetForm()
+}
+
+function getValues() { 
+    return {
+        name: nameEl.value,
+        surname: surnameEl.value,
+        phone: phoneEl.value,
+    }
+}
+function addContact(contact) { 
+    const rowEl = generateContactElement(contact)
+
+    contactListEl.append(rowEl)
+}
+
+function generateContactElement({name, surname, phone}){
+    const trEl = document.createElement('tr');
+
+    trEl.append(createCell(name))
+    trEl.append(createCell(surname))
+    trEl.append(createCell(phone))
+    trEl.append(createCell(''));
+
+    return trEl;
+}
+function createCell(value){
+    const tdEl = document.createElement('td');
+
+    tdEl.textContent = value;
+
+    return tdEl;
+}
+
+function resetForm() { 
+    nameEl.value = '';
+     surnameEl.value = '';
+     phoneEl.value = '';
+}
+
+function validateValues() { 
+    resetValidation()
+    if (nameEl.value === '') { 
+        nameEl.classList.add('invalid-input')
+        return false
+    };
+    if (surnameEl.value === '') { 
+        surnameEl.classList.add('invalid-input')
+        return false
+    };
+    if (phoneEl.value === '') { 
+        phoneEl.classList.add('invalid-input')
+        return false
+    };
+ return true 
+}
+
+function resetValidation() { 
+         nameEl.classList.remove('invalid-input')
+        surnameEl.classList.remove('invalid-input')
+        phoneEl.classList.remove('invalid-input')
+}
+
+
+
+    
+
+

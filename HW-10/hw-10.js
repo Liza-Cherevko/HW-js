6313af594925d6fc6c8af3bcad52ee2e7d308e9a
Liza <cherevkoelp@gmail.com>
2022-09-28 16:55:48 +0300

    commit

--- HW-10/hw-10.js
+++ HW-10/hw-10.js
@@ -1,6 +1,7 @@
 const TASK_DONE_CLASS = 'done';
 const TASK_ITEM_CLASS = 'task-item';
 const DELETE_BTN_CLASS = 'delete-btn';
+
 const contactListEl = document.querySelector('#contactsList');
 const nameEl = document.querySelector('#name');
 const surnameEl = document.querySelector('#surname');
@@ -8,13 +9,53 @@ const phoneEl = document.querySelector('#phone');
 const addBtnEl = document.querySelector('#addContactBtn');
 const newContactForm = document.querySelector('#newContactForm')
 let templateItem = document.querySelector('#newContactItemTemplate').innerHTML;
+const errorMsEl = document.querySelector('#errorMsEl');
 
-
-
+addBtnEl.addEventListener('click', onTaskNameInput)
 newContactForm.addEventListener('submit', onFormSubmit);
 contactListEl.addEventListener('click', onListClick);
 
 
+function onTaskNameInput(){
+  vilidateInput()
+}
+function validateInput(){
+    const value = contactListEl.value;
+
+    return validateValues(value);
+}
+
+
+function validateValues() { 
+    resetValidation()
+    if (nameEl.value === '') {
+        errorMsEl.textContent = 'ERROR. Write down the contact\'s name.';
+        addBtnEl.diabled = true;
+        return false;
+    }
+    if (surnameEl.value === '') {
+        errorMsEl.textContent = 'ERROR. Write down the contact\'s surname.';
+        addBtnEl.diabled = true;
+        return false;
+    }
+    if (phoneEl.value === '' || isNaN(phoneEl.value)) {
+        errorMsEl.textContent = 'ERROR. Write down the contact\'s phone. Use only number.';
+        addBtnEl.diabled = true;
+        return false;
+    }else {
+        errorMsEl.textContent = '';
+        addBtnEl.diabled = false;
+        return true;
+    }
+}
+
+function getValues() { 
+    return {
+        name: nameEl.value,
+        surname: surnameEl.value,
+        phone: phoneEl.value,
+    }
+}
 function onFormSubmit(event){
     event.preventDefault();
     if (!validateValues()) { 
@@ -26,21 +67,15 @@ function onFormSubmit(event){
 }
 
 function onListClick(event){
-    if (event.target.classList.contains(DELETE_BTN_CLASS)) {
+    if(event.target.classList.contains(DELETE_BTN_CLASS)){
         deleteContact(event.target.parentElement);
-    } else if (event.target.classList.contains(TASK_ITEM_CLASS)) {
-        toggleContact(event.target);
     }
 }
 
-
-function getValues() { 
-    return {
-        name: nameEl.value,
-        surname: surnameEl.value,
-        phone: phoneEl.value,
-    }
+function deleteContact(contact) {
+    contact.remove();
 }
+
 function addContact(contact) { 
     const contactHtml = generateContactFormHtml(contact) 
     contactListEl.insertAdjacentHTML('beforeend', contactHtml)
@@ -64,22 +99,6 @@ function resetForm() {
      phoneEl.value = '';
 }
 
-function validateValues() { 
-    resetValidation()
-    if (nameEl.value === '') { 
-        nameEl.classList.add('invalid-input')
-        return false
-    };
-    if (surnameEl.value === '') { 
-        surnameEl.classList.add('invalid-input')
-        return false
-    };
-    if (phoneEl.value === '') { 
-        phoneEl.classList.add('invalid-input')
-        return false
-    };
- return true 
-}
 
 function resetValidation() { 
          nameEl.classList.remove('invalid-input')
@@ -90,9 +109,7 @@ function resetValidation() {
 function toggleContact(contactEl) {
     contactEl.classList.toggle(TASK_DONE_CLASS);
 }
-function deleteContact(todoEl) {
-    todoEl.remove();
-}
+
 
 
 

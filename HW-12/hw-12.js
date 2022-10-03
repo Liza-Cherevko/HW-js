02db50fbde2c015731dfdd569d3421221b220da1
Liza-Cherevko <71131183+Liza-Cherevko@users.noreply.github.com>
2022-10-03 13:42:33 +0300

    hw-12, first commit

---
+++ HW-12/hw-12.js
@@ -0,0 +1,20 @@
+
+function Calculate(a) { 
+    let result = a;
+    this.add = function (val) { 
+        return result+= val
+    }
+    this.sub = function (val) { 
+        return result-= val
+    }
+    this.mul = function (val) { 
+        return result*= val
+    }
+    this.div = function (val) { 
+        return result/= val
+    }
+    this.set = function (val) { 
+        return result= val
+    }
+}
+let calc = new Calculate(100);

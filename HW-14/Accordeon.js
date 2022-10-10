7294ea2e5f036b284b952de54535f16091aca653
Liza <cherevkoelp@gmail.com>
2022-10-10 23:00:04 +0300

    commit

--- HW-14/Accordeon.js
+++ HW-14/Accordeon.js
@@ -1,54 +1,25 @@
 class Accordeon {
   #el = null;
-  #accItem = null;
-  #accTitle = null;
-  #accBody = null;
+ #boxElem = document.querySelectorAll('.accordeon-item ')
 
 constructor(el){
     this.#el = el;
-    this.#getElem();
-    this.#bindEventListeners();
+    this.#bindEventListeners()
     this.show(0);
-}
-#getElem(){
-   this.#accItem = document.querySelector('.accordeon-item');
-   this.#accTitle = document.querySelector('.accordeon-title');
-   this.#accBody = document.querySelector('.accordeon-body');
 }
 
 #bindEventListeners(){
-    this.#accItem.addEventListener('click', (e) => {
-        if (e.target.classList.contains('accordeon-body')) {
-            const index = this.#findTitleIndex(e.target);
-
-            this.show(index);
-        }
-    });
+    for (let i = 0; i < this.#boxElem.length; i++) {
+        this.#boxElem.length[i].addEventListener("click", function() {
+          this.classList.toggle("active");
+          let panel = this.nextElementSibling;
+          if (panel.style.display === "block") {
+            panel.style.display = "none";
+          } else {
+            panel.style.display = "block";
+          }
+        });
+      }
   }
-  #findTitleIndex(accItems){
-    for (let i = 0; i < this.#accItem.children.length; i++) {
-        if (this.#accItem.children[i] === accItems) {
-            return i;
-        }
-    }
-
-    return null;
-}
-show(index) {
-if (!this.#accItem.children[index]) {
-    return;
-}
-
-this.#hideAll();
-
-this.#accTitle.children[index].classList.add('acc-active');
-this.#accBody.children[index].classList.add('tacc-active');
-}
-
-#hideAll() {
-const visible = this.#el.querySelectorAll('.tabset-active');
-
-visible.forEach((el) => el.classList.remove('tabset-active'));
-}
-
+  
 }

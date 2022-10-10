80efeb9d001f5f26634a17850fe3f9f2d4923afc
Liza-Cherevko <71131183+Liza-Cherevko@users.noreply.github.com>
2022-10-10 14:51:19 +0300

    commit

--- HW-13/hw-13.js
+++ HW-13/hw-13.js
@@ -71,6 +71,8 @@ console.log('Current callories:'+ ham.getCallories());
 // function Hamburger(size ) {
 //   this._size = size;
 //   this._toppings = [];
+//   this._price = size.price;
+//   this._callories = size.calloriesce;
 // }
 // Hamburger.SIZE_SMALL= { price: 50, callories: 20};
 // Hamburger.SIZE_AVARAGE= { price: 75, callories: 30 };
@@ -82,19 +84,27 @@ console.log('Current callories:'+ ham.getCallories());
 // Hamburger.TOPPING_SPICE = { price: 15, callories: 0 };
 
 // Hamburger.prototype.addTopping = function (topping) {
-//  this._toppings.push(topping)
+//   this._toppings.push(topping)
+//   this._recalculate();
+// }
+
+// Hamburger.prototype._recalculate = function () { 
+//   this._price =  this._toppings.reduce((acc, { price}) => (acc+=price), this._size.price);
+//   this._callories =this._toppings.reduce((acc, { callories}) => (acc+=callories), this._size.callories);
 // }
 
 // Hamburger.prototype.getPrice = function () {
 //   // let price = 0;
 //   // price = +this._size.price;
 //   // this._toppings.forEach((topping) => (price += topping.price));
-//   return this._toppings.reduce((acc, { price}) => (acc+=price), this._size.price);
+//   // return this._toppings.reduce((acc, { price}) => (acc+=price), this._size.price);
+//   return this._price;
  
 
 // }
 // Hamburger.prototype.getCallories = function () {
-//   return this._toppings.reduce((acc, { callories}) => (acc+=callories), this._size.callories);
+//   // return this._toppings.reduce((acc, { callories}) => (acc+=callories), this._size.callories);
+//   return this._callories;
 // }
 
 // const ham = new Hamburger(Hamburger.SIZE_LARGE)

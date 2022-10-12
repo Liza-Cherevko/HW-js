8d9c48d53c7bb1907775a2b267f94f3716419a9d
Liza <cherevkoelp@gmail.com>
2022-10-12 13:07:13 +0300

    commit

--- HW-14/Accordeon.js
+++ HW-14/Accordeon.js
@@ -1,75 +1,75 @@
-class Accordeon {
-    #el = null;
-    #accItem = null; 
-    constructor(el) {
-        this.#el = el;
-        this.#bindEventListeners();
-        this.show(0);
-    }
 
-    #bindEventListeners() {
-        this.#accItem = document.querySelectorAll('.accordeon-item');
-        this.#accItem.addEventListener('click', (e) => {
-            if (e.target.classList.contains('accordeon-content')) {
-                const index = this.#findTitleIndex(e.target);
-
-                this.show(index);
-            }
-        });
-    }
-    #findTitleIndex(accEl) {
-        for (let i = 0; i < Accordeon.accItem.children.length; i++) {
-            if (Accordeon.accItem.children[i] === accEl) {
-                return i;
-            }
-        }
-
-        return null;
-    }
-    show(index) {
-        if (!Accordeon.accItem.children[index]) {
-            return;
-        }
-        this.#hideAll();
-
-       Accordeon.accItem[index].classList.add('active');
-    }
-    
-#hideAll() {
-    const visible = this.#el.querySelectorAll('.active');
-
-    visible.forEach((el) => el.classList.remove('active'));
-}
-}
 
 // рабочая версия
 
+class Accordeon {
+    #el = null;
+    static accItem = Array.from(document.querySelectorAll('.accordeon-item'));
+    constructor(el) {
+        this.#el = el;
+        this.#getElem()
+    
+    }
+    
+    #getElem() {
+        Accordeon.accItem.forEach((item) => {
+            item.addEventListener('click', (e) => {
+                let currentAcc = e.target.closest('.accordeon-item');
+        let currentContent = e.target.nextElementSibling;
+           currentAcc.classList.toggle('active');
+           if (currentAcc.classList.contains('active')) {
+               currentContent.style.maxHeight = currentContent.scrollHeight + 'px';
+           } else {
+            currentContent.style.maxHeight = 0
+           }
+            })
+});
+    }
+}
+
 // class Accordeon {
 //     #el = null;
-//     static accItem = Array.from(document.querySelectorAll('.accordeon-item'));
+//     #accItem = null; 
 //     constructor(el) {
 //         this.#el = el;
-//         this.#getElem()
-    
+//         this.#bindEventListeners();
+//         this.show(0);
+//     }
+
+//     #bindEventListeners() {
+//         this.#accItem = document.querySelectorAll('.accordeon-item');
+//         this.#accItem.addEventListener('click', (e) => {
+//             if (e.target.classList.contains('accordeon-content')) {
+//                 const index = this.#findTitleIndex(e.target);
+
+//                 this.show(index);
+//             }
+//         });
+//     }
+//     #findTitleIndex(accEl) {
+//         for (let i = 0; i < Accordeon.accItem.children.length; i++) {
+//             if (Accordeon.accItem.children[i] === accEl) {
+//                 return i;
+//             }
+//         }
+
+//         return null;
+//     }
+//     show(index) {
+//         if (!Accordeon.accItem.children[index]) {
+//             return;
+//         }
+//         this.#hideAll();
+
+//        Accordeon.accItem[index].classList.add('active');
 //     }
     
-//     #getElem() {
-//         Accordeon.accItem.forEach((item) => {
-//             item.addEventListener('click', (e) => {
-//                 let currentAcc = e.target.closest('.accordeon-item');
-//         let currentContent = e.target.nextElementSibling;
-//            currentAcc.classList.toggle('active');
-//            if (currentAcc.classList.contains('active')) {
-//                currentContent.style.maxHeight = currentContent.scrollHeight + 'px';
-//            } else {
-//             currentContent.style.maxHeight = 0
-//            }
-//             })
-// });
-//     }
+// #hideAll() {
+//     const visible = this.#el.querySelectorAll('.active');
+
+//     visible.forEach((el) => el.classList.remove('active'));
 // }
-
-
+// }
 
 
 

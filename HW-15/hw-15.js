ec9d50e227fa909a434ae33ac536858c17067105
Liza-Cherevko <71131183+Liza-Cherevko@users.noreply.github.com>
2022-10-18 13:58:42 +0300

    hw15

--- HW-15/hw-15.js
+++ HW-15/hw-15.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://5dd3d5ba8b5e080014dc4bfa.mockapi.io/todos/';
+const API_URL = 'https://jsonplaceholder.typicode.com/todos?_limit=10';
 const TASK_ITEM_CLASS = 'task-item';
 const TASK_ITEM_SELECTOR = '.' + TASK_ITEM_CLASS;
 const DELETE_BTN_CLASS = 'delete-btn'
@@ -57,11 +57,11 @@ function renderList(list){
   taskListEl.innerHTML =list.map(generateTodoItemHtml).join('')
 }
 
-function generateTodoItemHtml({id, title, isDone }){
+function generateTodoItemHtml({id, title, completed }){
   return todoItemTemplate
   .replaceAll('{{title}}', title )
   .replaceAll('{{id}}', id )
-  .replaceAll('{{isDone }}', isDone  ? TASK_DONE_CLASS :'' )
+  .replaceAll('{{completed }}', completed  ? TASK_DONE_CLASS :'' )
 }
 
 function getFormValues(){
@@ -71,20 +71,11 @@ function getFormValues(){
 }
 
 function addTodo(todo){
-    todo.isDone = false;
-    fetch(API_URL, {
-        method: 'POST',
-        body: JSON.stringify(todo),
-        headers: {
-            'Content-Type': 'application/json',
-        }
-    }).then((res) => res.json())
-     .then((data) => { 
-        todoList = [...todoList, data];
-        renderList(todoList);
-    })
-   
-
+    todo.id = Date.now();
+    todo.completed = false;
+    // todoList.push(todo)
+    todoList = [...todoList, todo]
+    renderList(todoList)
   
 }
 function resetForm(){
@@ -93,34 +84,134 @@ function resetForm(){
 
 function deleteTodo(id){
     todoList = todoList.filter((item)=> item.id !== id)
-    renderList(todoList)
-    fetch(API_URL + id, {
-        method: 'DELETE',
-    });
+ renderList(todoList)
 }
-
-function toggleTodo(id) {
-    const item = todoList.find((item) => item.id === id);
-    const updatedItem = {
-        ...item,
-        isDone: !item.isDone,
-    };
+function toggleTodo(id){
+    todoList = todoList.map((item)=> item.id !== id
+    ? item : {...item, completed: !item.completed});
    
-
-    fetch(API_URL + id, {
-        method: 'PUT',
-        body: JSON.stringify(updatedItem),
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    }).then(() => { 
-        todoList = todoList.map((item) => (item.id !== id ? item : updatedItem));
-        renderList(todoList);
-    })
+     renderList(todoList)
 }
-
-
 function getTodoId(el){
 const parent = el.closest(TASK_ITEM_SELECTOR );
-return parent ? parent.dataset.todoId : null;
+return parent ? +parent.dataset.todoId : null;
 }
+
+
+
+
+
+
+
+// const API_URL = 'https://jsonplaceholder.typicode.com/todos?_limit=10';
+// const TASK_ITEM_CLASS = 'task-item';
+// const TASK_ITEM_SELECTOR = '.' + TASK_ITEM_CLASS;
+// const DELETE_BTN_CLASS = 'delete-btn'
+// const TASK_DONE_CLASS = 'done';
+
+// const taskListEl = document.querySelector('#taskList');
+// const taskNameInput = document.querySelector('#taskNameInput');
+// const newTaskForm = document.querySelector('#newTaskForm');
+// const errorContainerEl = document.querySelector('#errorContainer');
+// const todoItemTemplate = document.querySelector('#todoItemTemplate').innerHTML;
+
+
+// newTaskForm.addEventListener('submit', onNewTaskFormSubmit)
+// taskListEl.addEventListener('click', onTaskListElClick)
+// let todoList = [
+//     {id:1, title:'Task1', isDone: true},
+//     {id:2, title:'Task2', isDone: false},
+//     {id:3, title:'Task3', isDone: true},
+// ];
+
+// init()
+
+// function init() {
+//     fetchTodoList().then(() => renderList(todoList));
+  
+// }
+
+// function fetchTodoList() {
+//     return fetch(API_URL)
+//         .then((res) => res.json())
+//         .then((data) => {
+//             todoList = data;
+//         });
+// }
+
+// function onNewTaskFormSubmit(e){
+//      e.preventDefault();
+//    const newTask = getFormValues();
+//    addTodo(newTask)
+//    resetForm()
+// }
+
+// function onTaskListElClick(e){
+//     const todoId =  getTodoId(e.target)
+//     // if(e.target.classList.contains(DELETE_BTN_CLASS)){
+//     //    deleteTodo(todoId);
+//     // }
+//     // if(e.target.classList.contains(TASK_ITEM_CLASS)){
+//     //    toggleTodo(todoId)
+//     // }
+
+//   switch(true){
+//         case e.target.classList.contains(DELETE_BTN_CLASS):
+//            return deleteTodo(todoId); 
+//         case e.target.classList.contains(TASK_ITEM_CLASS):
+//            return toggleTodo(todoId) ;
+//   }
+
+//    }
+
+// function renderList(list){
+// //   const htmls = list.map(generateTodoItemHtml);
+//   taskListEl.innerHTML =list.map(generateTodoItemHtml).join('')
+
+// }
+
+// function generateTodoItemHtml({id, title, completed}){
+//   return todoItemTemplate
+//   .replaceAll('{{title}}', title )
+//   .replaceAll('{{id}}', id )
+//   .replaceAll('{{completed}}', completed ? TASK_DONE_CLASS :'' )
+// }
+
+// function getFormValues(){
+//     return{
+//         title: taskNameInput.value,
+//     }
+// }
+
+// function addTodo(todo){
+//     todo.id = Date.now();
+//     todo.completed = false;
+//     // todoList.push(todo)
+//     todoList = [...todoList, todo]
+//     renderList(todoList)
+  
+// }
+// function resetForm(){
+//     taskNameInput.value = '';
+// }
+
+// function deleteTodo(id){
+//     todoList = todoList.filter((item)=> item.id !== id)
+//  renderList(todoList)
+// }
+// function toggleTodo(id){
+// //        const todo = todoList.find((item) =>item.id === id);
+// //   todo.isDone = !todo.isDone;
+// // const updatedTodo = {
+// //     ...todo,
+// //     isDone: !todo.isDone,
+// // }
+// todoList = todoList.map((item)=> item.id !== id
+//  ? item : {...item, completed: !item.completed});
+
+//   renderList(todoList)
+// }
+// function getTodoId(el){
+// const parent = el.closest(TASK_ITEM_SELECTOR );
+// return parent ? +parent.dataset.todoId : null;
+// }

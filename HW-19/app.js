52ca4057b4b07bd8a62258deeda0c2b58ae845c6
Liza <cherevkoelp@gmail.com>
2022-11-12 15:54:10 +0200

    commit

--- HW-19/app.js
+++ HW-19/app.js
@@ -2,35 +2,35 @@
 
 
 const API_URL = 'https://jsonplaceholder.typicode.com/photos?_limit=10';
-let $fotoramaDiv = $('#fotorama').fotorama({
-    data: preparedData
-});
-let fotorama = $fotoramaDiv.data('fotorama');
-console.log(fotorama);
+// let $fotoramaDiv = $('#fotorama').fotorama({
+//     data: preparedData
+// });
+// let fotorama = $fotoramaDiv.data('fotorama');
+// console.log(fotorama);
 
-function preparedData(){
-    fetch(API_URL).then(response => response.json())
-        .then(data => {
-            data.map(photo => img= photo.url, thumb= photo.thumbnailUrl );
-        });
-}
-
-// function init() {
+// function preparedData(){
 //     fetch(API_URL).then(response => response.json())
 //         .then(data => {
-//             data.forEach(photo => {
-//                 $('.swipebox').append(
-//                     ` <a data-src="${photo.url}" data-caption="${photo.title}" rel="gallery-1"  class="swipebox">
-//                     <img src="${photo.thumbnailUrl}" alt="image"/> 
-//                     </a>`
-//                 ).swipebox();
-//             });
+//             data.map(photo => img= photo.url, thumb= photo.thumbnailUrl );
 //         });
-// };
+// }
+
+function init() {
+    fetch(API_URL).then(response => response.json())
+        .then(data => {
+            data.forEach(photo => {
+                $('.fotorama').append(
+                    ` <a data-src="${photo.url}" data-caption="${photo.title}" rel="gallery-1"  class="swipebox">
+                    <img src="${photo.thumbnailUrl}" alt="image"/> 
+                    </a>`
+                );
+            });
+        });
+};
 
 
 
-// init()
+init()
 // function init() {
 //     fetch(API_URL).then(response => response.json())
 //         .then(data => {

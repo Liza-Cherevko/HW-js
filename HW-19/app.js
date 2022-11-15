a37f1f2a94d3bd99e54c78f154d4e2059bc29f02
Liza-Cherevko <71131183+Liza-Cherevko@users.noreply.github.com>
2022-11-15 16:07:43 +0200

    hw-19

--- HW-19/app.js
+++ HW-19/app.js
@@ -5,7 +5,8 @@
 const API_URL = 'https://jsonplaceholder.typicode.com/photos?_limit=10';
 init();
 function init() {
-    fetch(API_URL).then(response => response.json())
+    fetch(API_URL)
+        .then(response => response.json())
         .then(data => {
             data.forEach(photo => {
                 $('.fotorama').append(

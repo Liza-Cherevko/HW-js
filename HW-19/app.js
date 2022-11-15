c5f5c529eacae4b11c0eb57fabb818261a2e8f3b
Liza-Cherevko <71131183+Liza-Cherevko@users.noreply.github.com>
2022-11-15 17:47:57 +0200

    hw-19

--- HW-19/app.js
+++ HW-19/app.js
@@ -1,47 +1,22 @@
-// const controller = new GalleryController($('.fotorama'));
+
 
 
 const API_URL = 'https://jsonplaceholder.typicode.com/photos?_limit=10';
-// let $fotoramaDiv = $('#fotorama').fotorama({
-//     data: preparedData
-// });
-// let fotorama = $fotoramaDiv.data('fotorama');
-// console.log(fotorama);
 
-// function preparedData(){
-//     fetch(API_URL).then(response => response.json())
-//         .then(data => {
-//             data.map(photo => img= photo.url, thumb= photo.thumbnailUrl );
-//         });
-// }
 
-function init() {
+
+function preparedData() {
     fetch(API_URL)
         .then(response => response.json())
-        .then(data => {
-            data.forEach(photo => {
-                $('.fotorama').append(
-                    ` <a data-src="${photo.url}" data-caption="${photo.title}" rel="gallery-1"  class="swipebox">
-                    <img src="${photo.thumbnailUrl}" alt="image"/> 
-                    </a>`
-                );
-            });
-        });
-};
+        .then(photos => {
+            $('.fotorama').fotorama({
+                data: photos.map(photo => ({ img: photo.url, thumb: photo.thumbnailUrl }))
+            })
+        })
+}
+preparedData()
 
 
 
-init()
-// function init() {
-//     fetch(API_URL).then(response => response.json())
-//         .then(data => {
-//             data.forEach(photo => {
-//                 $('.swipebox').append(
-//                     ` <a data-src="${photo.url}" data-caption="${photo.title}" rel="gallery-1"  class="swipebox">
-//                     <img src="${photo.thumbnailUrl}" alt="image"/> 
-//                     </a>`
-//                 ).swipebox();
-//             });
-//         });
-// };
+
 

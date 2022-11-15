f21487b817b3b499748593df989231af46883a24
Liza-Cherevko <71131183+Liza-Cherevko@users.noreply.github.com>
2022-11-15 16:13:50 +0200

    hw-20

--- HW-20/utils/add.js
+++ HW-20/utils/add.js
@@ -1,25 +1,4 @@
-module.exports = function add(a, b){
- let result = a + b;
- return result 
+module.exports = function add(a, b) { 
+    return a+b
 }
 
-
-// function Calculate(a) { 
-//     let result = a;
-//     this.add = function (val) { 
-//         return result+= val
-//     }
-//     this.sub = function (val) { 
-//         return result-= val
-//     }
-//     this.mul = function (val) { 
-//         return result*= val
-//     }
-//     this.div = function (val) { 
-//         return result/= val
-//     }
-//     this.set = function (val) { 
-//         return result= val
-//     }
-// }
-// let calc = new Calculate(100);
